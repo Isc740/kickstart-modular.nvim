acc2e2a3557e0dbee7824b0e79f584094df6a158
Isc740 <isaazcantillo@gmail.com>
2024-10-15 16:10:51 -0500

    add css lsp and linting support

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -191,6 +191,7 @@ return {
         biome = {},
         denols = {},
         eslint = {},
+        cssls = {},
         -- ruby_lsp = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -231,6 +232,8 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'htmlhint',
+        'stylelint',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
